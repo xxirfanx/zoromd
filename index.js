a7322549cd6bd40d9aa8c07da9fce0c4fd9919e9
LUA HACKER BRO <63995922+xxirfanx@users.noreply.github.com>
2021-03-11 05:36:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log('Starting Bot...')
+let { spawn } = require('child_process')
+let path = require('path')
+let fs = require('fs')
+let package = require('./package.json')
+const CFonts  = require('cfonts')
+CFonts.say('DNS BOT', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+CFonts.say(`${package.name}`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
+})
+function start(file) {
+  let args = [path.join(file), ...process.argv.slice(2)]
+  CFonts.say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  })
+  let p = spawn(process.argv[0], args, {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+  })
+  .on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  .on('error', e => {
+    console.error(e)
+    fs.watchFile(args[0], () => {
+      start()
+      fs.unwatchFile(args[0])
+    })
+  })
+  // console.log(p)
+}
+start('denz.js')
