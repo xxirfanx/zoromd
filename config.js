90bb9b2c1e71522c6bef0cc3bfef5508d540f9da
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-01 22:35:01 +0530

    [ zoromd ]

---
+++ config.js
@@ -0,0 +1,97 @@
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import { watchFile, unwatchFile, readFileSync } from 'fs'
+
+global.owner = 
+ [
+  ['+48 888 880 215', '𝖑𝖚𝖆 𝖘𝖊𝖗 𝖔𝖋𝖈', true]
+]
+// Put your number here
+// [number, he creator/owner?, he developer?]
+
+global.mods = [] // Want some help?
+global.prems = [] // Premium user has unlimited limit
+
+// api key here okay
+global.xyro = 'LyrK9JMI7N' // https://api.xyroinee.xyz free limit unlimited
+global.apilol = 'BrunoSobrino_2' // https://api.lolhuman.xyz free limit unlimited by @BrunoSobrino
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b'
+}
+
+ global.thumbnailUrl = [
+  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
+  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
+  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
+  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
+  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+]
+
+// Sticker WM
+global.packname = `「 Cʀᴇᴀᴛᴇᴅ Bʏ 𝖑𝖚𝖆 𝖘𝖊𝖗 𝖔𝖋𝖈 」`
+global.author = ''
+global.thumb = readFileSync('./me.png')
+global.thumb2 = readFileSync('./me2.jpeg')
+global.multiplier = 69 // The higher, The harder levelup
+
+//Text here
+global.me = 'Cʀᴇᴀᴛᴇᴅ Bʏ 𝖑𝖚𝖆 𝖘𝖊𝖗 𝖔𝖋𝖈 - 2023'
+global.bott = 'zoro md'
+global.nomorown = '48888880215'
+// owner put this creator.js
+global.str = '-------------'
+global.l = '「'
+global.r = '」'
+global.wait = '```「▰▰▰▱▱▱▱▱▱▱」ʟ ᴏ ᴀ ᴅ ɪ ɴ ɢ...```'
+global.eror = '```404 ᴇʀʀᴏʀ```'
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '🧬',
+      limit: '🌌',
+      health: '❤️',
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      trash: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      wood: '🪵',
+      rock: '🪨',
+      string: '🕸️',
+      horse: '🐎',
+      cat: '🐈',
+      dog: '🐕',
+      fox: '🦊',
+      petFood: '🍖',
+      iron: '⛓️',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.cyanBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
