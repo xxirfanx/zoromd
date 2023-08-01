6d7c3f01a1842082828ef2b3985a44f9a3f9ca15
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-02 03:04:08 +0530

    zoromd

--- plugins/sc.js
+++ plugins/sc.js
@@ -18,7 +18,7 @@ let buttonMessage = {
 'body': me,
 'thumbnail': thumb2,
 'sourceUrl': 'https://youtu.be/F5WxZTMFbk0'}},
-'caption': 'http://github.com/xxirfanx/zoromd\nOpen Source for free\n𝑺𝒊𝒎𝒑𝒍𝒆 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑 𝑩𝒐𝒕 𝑴𝒖𝒍𝒕𝒊-𝒅𝒆𝒗𝒊𝒄𝒆 𝑫𝒐𝒏\'𝒕 𝒇𝒐𝒓𝒈𝒐𝒕 𝒕𝒐 𝒈𝒊𝒗𝒆 𝒔𝒕𝒂𝒓 𝒊𝒇 𝒂𝒏 𝒇𝒖𝒏𝒄𝒕𝒊𝒐𝒏 𝒐𝒇 𝒂𝒏 𝒇𝒊𝒍𝒆. 𝑻𝒉𝒂𝒏𝒌𝒔',
+'caption': 'http://github.com/xxirfanx/zoromd\n\nOpen Source for free\n\n𝑺𝒊𝒎𝒑𝒍𝒆 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑 𝑩𝒐𝒕 𝑴𝒖𝒍𝒕𝒊-𝒅𝒆𝒗𝒊𝒄𝒆 𝑫𝒐𝒏\'𝒕 𝒇𝒐𝒓𝒈𝒐𝒕 𝒕𝒐 𝒈𝒊𝒗𝒆 𝒔𝒕𝒂𝒓 𝒊𝒇 𝒂𝒏 𝒇𝒖𝒏𝒄𝒕𝒊𝒐𝒏 𝒐𝒇 𝒂𝒏 𝒇𝒊𝒍𝒆. 𝑻𝒉𝒂𝒏𝒌𝒔',
 'footer': me, }
     await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }
