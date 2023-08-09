559902b92dc81c215ce8cfecf91ff4b064afa0a9
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-09 11:40:21 +0530

    Update replit.nix version new

--- replit.nix
+++ replit.nix
@@ -1,22 +1,11 @@
 { pkgs }: {
-    deps = [
-        pkgs.nodejs-16_x
-        pkgs.speedtest_cli
-        pkgs.yarn
-        pkgs.arcan.ffmpeg
-        pkgs.openssh_with_kerberos
-        pkgs.nodePackages.typescript
-        pkgs.nodePackages.pm2
-        pkgs.imagemagick
-        pkgs.libwebp
-        pkgs.git
-        pkgs.python2
-        pkgs.python310Packages.python
-        pkgs.libuuid
-    ];
-    env = {
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.libuuid
-        ];
-    };
+	deps = [
+    pkgs.nodejs_20
+    pkgs.neofetch
+		pkgs.imagemagick
+		pkgs.speedtest-cli
+		pkgs.jellyfin-ffmpeg
+		pkgs.git
+		pkgs.python311Packages.python
+	];
 }
