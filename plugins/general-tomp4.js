9a0f83da4799b6749a98dca184dbc7c232c80acf
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-16 10:07:41 +0530

    ptv to video support

--- plugins/general-tomp4.js
+++ plugins/general-tomp4.js
@@ -1,18 +1,17 @@
 import { webp2mp4 } from '../lib/webp2mp4.js'
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-	const notStickerMessage = `Reply sticker with command *${usedPrefix + command}*`
-	if (!m.quoted) throw notStickerMessage
+	if (!m.quoted) throw `Reply sticker / ptv with command *${usedPrefix + command}*`
 	const q = m.quoted || m
 	let mime = q.mediaType || ''
-	if (!/sticker/.test(mime)) throw notStickerMessage
+	if (!/sticker|ptv/.test(mime)) throw `Reply sticker / ptv with command *${usedPrefix + command}*`
 	let media = await q.download()
-	let out = await webp2mp4(media).catch(_ => null) || Buffer.alloc(0)
+	let out = /ptv/.test(mime) ? media : await webp2mp4(media).catch(_ => null) || Buffer.alloc(0)
 	await conn.sendFile(m.chat, out, 'out.mp4', '*DONE*', m)
 }
 
-handler.help = ['tomp4']
+handler.help = ['ptvtovideo','tomp4']
 handler.tags = ['tools']
-handler.command = /^(tomp4|tovideo)$/i
+handler.command = /^((ptv)?to(mp4|video))$/i
 
 export default handler
