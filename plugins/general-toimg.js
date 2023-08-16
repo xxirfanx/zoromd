e750babd9afe8b70bd32d08d6f217296b0e79be9
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-16 10:36:50 +0530

    fix img bug

--- plugins/general-toimg.js
+++ plugins/general-toimg.js
@@ -1,23 +1,30 @@
 import { spawn } from 'child_process'
+import { webp2png } from '../lib/webp2mp4.js'
 
-let handler = async (m, { conn, usedPrefix }) => {
-	if (m.quoted && /sticker/.test(m.quoted.mtype) && !m.quoted.isAnimated) {
-		let q = m.quoted
-		let sticker = await q.download()
-		if (!sticker) throw sticker
-		let bufs = []
-		const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
-		let im = spawn(_spawnprocess, _spawnargs)
-		im.on('error', e => m.reply(format(e)))
-		im.stdout.on('data', chunk => bufs.push(chunk))
-		im.stdin.write(sticker)
-		im.stdin.end()
-		im.on('exit', async () => {
-			await conn.sendMsg(m.chat, { image: Buffer.concat(bufs), caption: `*DONE*` }, { quoted: m })
-		})
-	} else if (m.quoted && /sticker/.test(m.quoted.mtype) && m.quoted.isAnimated) {
-		return m.reply(`well failed\nTry using *${usedPrefix}tomp4* for moving stickers`)
-	} else throw 'Reply / tag Sticker'
+let handler = async (m, { conn, command, usedPrefix }) => {
+	let q = m.quoted
+	try {
+		if (q && /sticker/.test(q.mtype)) {
+			if (q.isAnimated) return m.reply(`Use *${usedPrefix}tomp4* for moving stickers`)
+			let sticker = await q.download()
+			if (!sticker) throw sticker
+			let bufs = []
+			const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []), 'convert', 'webp:-', 'png:-']
+			let im = spawn(_spawnprocess, _spawnargs)
+			im.on('error', e => m.reply(format(e)))
+			im.stdout.on('data', chunk => bufs.push(chunk))
+			im.stdin.write(sticker)
+			im.stdin.end()
+			im.on('exit', async () => {
+				await conn.sendMsg(m.chat, { image: Buffer.concat(bufs), caption: `*DONE*` }, { quoted: m })
+			})
+		} else return m.reply('Reply / tag Sticker')
+	} catch (e) {
+		console.log(e)
+		let media = await q.download()
+		let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+		await conn.sendMsg(m.chat, { image: out, caption: `*DONE*` }, { quoted: m })
+	}
 }
 
 handler.help = ['toimg']
