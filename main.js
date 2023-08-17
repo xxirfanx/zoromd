b728688bbc0fbdd247c7cc7cc4ff4d64d4aa72db
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-17 19:53:48 +0530

    fix ffmpeg, fix clearTmp, fix plugins, hehe

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import path, { join } from 'path'
 import { platform } from 'process'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
-import { createRequire } from 'module' // Bring in the ability to create the 'require' method
+import { createRequire } from 'module'
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
 
 import * as ws from 'ws'
@@ -126,28 +126,26 @@ global.conn = makeWASocket(connectionOptions)
 conn.isInit = false
 
 if (!opts['test']) {
-    setInterval(async () => {
-        if (global.db.data) await global.db.write().catch(console.error)
-        if (opts['autocleartmp']) try {
-            clearTmp()
-
-        } catch (e) {
-            console.error(e)
-        }
-    }, 30 * 1000)
-
+    if (global.db) {
+        setInterval(async () => {
+            if (global.db.data) await global.db.write().catch(console.error)
+            // if (opts['autocleartmp'] && (global.support || {}).find)(tmp = [os.tmpdir(), 'tmp'], tmp.forEach((filename) => cp.spawn('find', [filename, '-amin', '3', '-type', 'f', '-delete'])));
+          clearTmp()
+        }, 30 * 1000);
+    }
 }
-if (opts['server'])(await import('./server.js')).default(global.conn, PORT)
+
+if (opts['server'])(await import('./server.js')).default(global.conn, PORT);
 
 function clearTmp() {
-  const tmp = [tmpdir(), join(__dirname, './tmp')]
-  const filename = []
-  tmp.forEach(dirname => readdirSync(dirname).forEach(file => filename.push(join(dirname, file))))
-  return filename.map(file => {
-    const stats = statSync(file)
-    if (stats.isFile() && (Date.now() - stats.mtimeMs >= 5 * 60 * 1000)) return unlinkSync(file);
-    return false
-  })
+    const tmp = [tmpdir(), join(__dirname, './tmp')];
+    const filename = [];
+    tmp.forEach((dirname) => readdirSync(dirname).forEach((file) => filename.push(join(dirname, file))));
+    return filename.map((file) => {
+        const stats = statSync(file);
+        if (stats.isFile() && (Date.now() - stats.mtimeMs >= 5 * 60 * 1000)) return unlinkSync(file);
+        return false;
+    });
 }
 
 async function connectionUpdate(update) {
@@ -160,34 +158,61 @@ async function connectionUpdate(update) {
   }
   // console.log(JSON.stringify(update, null, 4))
   if (global.db.data == null) loadDatabase()
-    if (connection === 'open') {
-        const authorText = `Made by ${me}`;
-        const lineLength = authorText.length + 6;
-        const line = '─'.repeat(lineLength);
-
-        console.log(chalk.yellow(`╭${line}╮\n│  ${authorText}  │\n╰${line}╯`));
+    if (connection === "open") {
+        const {
+            jid,
+            name
+        } = conn.user;
+        const currentTime = new Date();
+        const pingStart = new Date();
+        const infoMsg = `
+   ℹ️ *Bot Info:*
+   
+   🕒 here and now: ${currentTime}
+   👤 Name: ${name || 'zoromd'}
+   🏷️ Tag: @${jid.split('@')[0]}
+   ⏱️ Speed ping: ${pingStart - new Date()}ms
+   📅 Date: ${currentTime.toDateString()}
+   🕰️ hour: ${currentTime.toLocaleTimeString()}
+   📅 Day: ${currentTime.toLocaleDateString('en-US', { weekday: 'long' })}
+   📝 Description: *Bot ${name || 'zoromd'} activated*.
+        `;
+        conn.sendMessage(nomorown + "@s.whatsapp.net", {
+            text: infoMsg,
+            mentions: [nomorown + "@s.whatsapp.net", jid]
+        }, {
+            quoted: null
+        })
+        chalk.yellow('R E A D Y');
+    }
+    if (connection == 'close') {
+        console.log(chalk.yellow(`🚩ㅤConnection closed, please delete the folder sessions and rescan the QR code`));
     }
 }
 
 process.on('uncaughtException', console.error)
 // let strQuot = /(["'])(?:(?=(\\?))\2.)*?\1/
 
-let isInit = true
-let handler = await import('./handler.js')
-global.reloadHandler = async function (restatConn) {
-  try {
-    const Handler = await import(`./handler.js?update=${Date.now()}`).catch(console.error)
-    if (Object.keys(Handler || {}).length) handler = Handler
-  } catch (e) {
-    console.error(e)
-  }
-  if (restatConn) {
-    const oldChats = global.conn.chats
-    try { global.conn.ws.close() } catch { }
-    conn.ev.removeAllListeners()
-    global.conn = makeWASocket(connectionOptions, { chats: oldChats })
-    isInit = true
-  }
+let isInit = true;
+let handler = await import('./handler.js');
+global.reloadHandler = async function(restatConn) {
+    try {
+        const Handler = await import(`./handler.js?update=${Date.now()}`).catch(console.error);
+        if (Object.keys(Handler || {}).length) handler = Handler;
+    } catch (error) {
+        console.error;
+    }
+    if (restatConn) {
+        const oldChats = global.conn.chats;
+        try {
+            global.conn.ws.close();
+        } catch {}
+        conn.ev.removeAllListeners();
+        global.conn = makeWaSocket(connectionOptions, {
+            chats: oldChats
+        });
+        isInit = true;
+    }
   if (!isInit) {
     conn.ev.off('messages.upsert', conn.handler)
     conn.ev.off('group-participants.update', conn.participantsUpdate)
@@ -197,7 +222,7 @@ global.reloadHandler = async function (restatConn) {
   }
 
   conn.welcome = 'Hai, @user!\nWelcome to @subject\n\n@desc'
-  conn.bye = 'Sayonara @user!'
+  conn.bye = 'GOOD BYE @user!'
   conn.spromote = '@user now admin!'
   conn.sdemote = '@user now not admin!'
   conn.handler = handler.handler.bind(global.conn)
@@ -205,6 +230,14 @@ global.reloadHandler = async function (restatConn) {
   conn.onDelete = handler.deleteUpdate.bind(global.conn)
   conn.connectionUpdate = connectionUpdate.bind(global.conn)
   conn.credsUpdate = saveCreds.bind(global.conn)
+
+  const currentDateTime = new Date();
+    const messageDateTime = new Date(conn.ev);
+    if (currentDateTime >= messageDateTime) {
+        const chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map((v) => v[0]);
+    } else {
+        const chats = Object.entries(conn.chats).filter(([jid, chat]) => !jid.endsWith('@g.us') && chat.isChats).map((v) => v[0]);
+    }
 
   conn.ev.on('messages.upsert', conn.handler)
   conn.ev.on('group-participants.update', conn.participantsUpdate)
@@ -215,77 +248,78 @@ global.reloadHandler = async function (restatConn) {
    return true
 }
 
-const pluginFolder = global.__dirname(join(__dirname, './plugins/index'))
-const pluginFilter = filename => /\.js$/.test(filename)
-global.plugins = {}
+const pluginFolder = global.__dirname(join(__dirname, './plugins/index'));
+const pluginFilter = (filename) => /\.js$/.test(filename);
+global.plugins = {};
 async function filesInit() {
-    for (let filename of readdirSync(pluginFolder).filter(pluginFilter)) {
+    for (const filename of readdirSync(pluginFolder).filter(pluginFilter)) {
         try {
-            let file = global.__filename(join(pluginFolder, filename))
-            const module = await import(file)
-            global.plugins[filename] = module.default || module
+            const file = global.__filename(join(pluginFolder, filename));
+            const module = await import(file);
+            global.plugins[filename] = module.default || module;
         } catch (e) {
-            conn.logger.error(e)
-            delete global.plugins[filename]
+            conn.logger.error(e);
+            delete global.plugins[filename];
         }
     }
 }
-filesInit().then(_ => console.log(Object.keys(global.plugins))).catch(console.error)
+filesInit().then((_) => Object.keys(global.plugins)).catch(console.error);
 
 global.reload = async (_ev, filename) => {
     if (pluginFilter(filename)) {
-        let dir = global.__filename(join(pluginFolder, filename), true)
+        const dir = global.__filename(join(pluginFolder, filename), true);
         if (filename in global.plugins) {
-            if (existsSync(dir)) conn.logger.info(` updated plugin - '${filename}'`)
+            if (existsSync(dir)) conn.logger.info(` updated plugin - '${filename}'`);
             else {
-                conn.logger.warn(`deleted plugin - '${filename}'`)
-                return delete global.plugins[filename]
+                conn.logger.warn(`deleted plugin - '${filename}'`);
+                return delete global.plugins[filename];
             }
-        } else conn.logger.info(`new plugin - '${filename}'`)
-        let err = syntaxerror(readFileSync(dir), filename, {
+        } else conn.logger.info(`new plugin - '${filename}'`);
+        const err = syntaxerror(readFileSync(dir), filename, {
             sourceType: 'module',
-            allowAwaitOutsideFunction: true
-        })
-        if (err) conn.logger.error(`syntax error while loading '${filename}'\n${format(err)}`)
-        else try {
-            const module = (await import(`${global.__filename(dir)}?update=${Date.now()}`))
-            global.plugins[filename] = module.default || module
-        } catch (e) {
-            conn.logger.error(`error require plugin '${filename}\n${format(e)}'`)
-        } finally {
-            global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)))
+            allowAwaitOutsideFunction: true,
+        });
+        if (err) conn.logger.error(`syntax error while loading '${filename}'\n${format(err)}`);
+        else {
+            try {
+                const module = (await import(`${global.__filename(dir)}?update=${Date.now()}`));
+                global.plugins[filename] = module.default || module;
+            } catch (e) {
+                conn.logger.error(`error require plugin '${filename}\n${format(e)}'`);
+            } finally {
+                global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)));
+            }
         }
     }
-}
-Object.freeze(global.reload)
-watch(pluginFolder, global.reload)
-await global.reloadHandler()
+};
+Object.freeze(global.reload);
+watch(pluginFolder, global.reload);
+await global.reloadHandler();
 
 /* QuickTest */
 async function _quickTest() {
-    let test = await Promise.all([
+    const test = await Promise.all([
         spawn('ffmpeg'),
         spawn('ffprobe'),
         spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-']),
         spawn('convert'),
         spawn('magick'),
         spawn('gm'),
-        spawn('find', ['--version'])
-    ].map(p => {
+        spawn('find', ['--version']),
+    ].map((p) => {
         return Promise.race([
-            new Promise(resolve => {
-                p.on('close', code => {
-                    resolve(code !== 127)
-                })
+            new Promise((resolve) => {
+                p.on('close', (code) => {
+                    resolve(code !== 127);
+                });
             }),
-            new Promise(resolve => {
-                p.on('error', _ => resolve(false))
+            new Promise((resolve) => {
+                p.on('error', (_) => resolve(false));
             })
-        ])
-    }))
-    let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test
-    console.log(test)
-    let s = global.support = {
+        ]);
+    }));
+    const [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test;
+    const s = global.support = {
         ffmpeg,
         ffprobe,
         ffmpegWebp,
@@ -293,16 +327,25 @@ async function _quickTest() {
         magick,
         gm,
         find
-    }
+    };
+    Object.freeze(global.support);
+}
+setInterval(async () => {
+    if (stopped === 'close' || !conn || !conn.user) return;
+    await clearTmp();
+    console.log(chalk.cyanBright(
+        `\n╭───────────────────────────────────···\n│\n` +
+        `│  Storage Setting Successful ✅\n│\n` +
+        `╰──────────────────────────────────────···\n`
+    ));
+}, 60 * 60 * 1000);
 
-    Object.freeze(global.support)
-
-    if (!s.ffmpeg) conn.logger.warn('Please install ffmpeg for sending videos (pkg install ffmpeg)')
-    if (s.ffmpeg && !s.ffmpegWebp) conn.logger.warn('Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
-    if (!s.convert && !s.magick && !s.gm) conn.logger.warn('Stickers may not work without imagemagick if libwebp on ffmpeg doesnt isntalled (pkg install imagemagick)')
+function clockString(ms) {
+    const d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
+    const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
+    const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+    const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+    return [d, ' Day ', h, ' hours ', m, ' Minutes ', s, ' Second '].map((v) => v.toString().padStart(2, '0')).join('');
 }
 
-/* QuickTest */
-_quickTest()
-    .then(() => conn.logger.info('Quick Test Done'))
-    .catch(console.error)
+_quickTest().catch(console.error);
