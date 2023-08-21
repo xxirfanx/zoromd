d2375f1b4921a5119aad677c530de99263cc299a
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-21 17:20:32 +0530

    fix bug fetch

--- plugins/tools-fetch.js
+++ plugins/tools-fetch.js
@@ -1,24 +1,72 @@
-import fetch from 'node-fetch'
-import { format } from 'util'
+import got from 'got';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import { format } from 'util';
+
+const MAX_CONTENT_SIZE = 100 * 1024 * 1024 * 1024; // 100GB
 
 let handler = async (m, { text }) => {
-	if (!/^https?:\/\//.test(text)) throw 'Param *URL* must be starts with http:// or https://'
-	let { href: url, origin } = new URL(text)
-	let res = await fetch(url, { headers: { 'referer': origin }})
-	if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) throw `Content-Length: ${res.headers.get('content-length')}`
-	if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
-	let txt = await res.buffer()
-	try {
-		txt = format(JSON.parse(txt + ''))
-	} catch (e) {
-		txt = txt + ''
-	} finally {
-		m.reply(txt.slice(0, 65536) + '')
-	}
-}
-handler.help = ['fetch']
-handler.tags = ['tools']
-handler.alias = ['get', 'fetch']
-handler.command = /^(fetch|get)$/i
+  if (!text) throw '*Enter the link*\n*Ex:* http:// or https://';
 
-export default handler
+  text = addHttpsIfNeeded(text);
+  let { href: url, origin } = new URL(text);
+  let response, txt;
+
+  try {
+    response = await got(url, { headers: { 'referer': origin } });
+    txt = response.body;
+  } catch {
+    try {
+      response = await fetch(url, { headers: { 'referer': origin } });
+      txt = await response.text();
+    } catch {
+      try {
+        response = await axios.get(url, { headers: { 'referer': origin } });
+        txt = response.data;
+      } catch {
+        throw "Failed to fetch data from all sources";
+      }
+    }
+  }
+
+  const contentLength = response.headers['content-length'];
+  if (contentLength > MAX_CONTENT_SIZE) {
+    return m.reply(`File too big. The maximum size is ${formatSize(MAX_CONTENT_SIZE)}`);
+  }
+
+  if (!/text|json/.test(response.headers['content-type'])) {
+    return conn.sendFile(m.chat, url, ucapan, author, m);
+  }
+
+  try {
+    txt = format(JSON.parse(txt + ''));
+  } catch {
+    txt = txt + '';
+  } finally {
+    m.reply(txt.slice(0, 65536) + '');
+  }
+};
+
+function addHttpsIfNeeded(link) {
+  if (!/^https?:\/\//i.test(link)) {
+    link = "https://" + link;
+  }
+  return link;
+}
+
+function formatSize(size) {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+  let i = 0;
+  while (size >= 1024 && i < 4) {
+    size /= 1024;
+    i++;
+  }
+  return `${size.toFixed(2)} ${units[i]}`;
+}
+
+handler.help = ['fetch'];
+handler.tags = ['tools'];
+handler.alias = ['get', 'fetch'];
+handler.command = /^(fetch|get)$/i;
+
+export default handler;
