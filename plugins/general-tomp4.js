3b99bc4d20f38b88e84efe9bbaede99e4c196763
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-26 00:02:41 +0530

    fix tomp4

--- plugins/general-tomp4.js
+++ plugins/general-tomp4.js
@@ -3,9 +3,9 @@ import { webp2mp4 } from '../lib/webp2mp4.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || q.mediaType || ''
-	if (/sticker/.test(mime) || /ptv/.test(q.mtype) || q.isAnimated) {
+	if (/webp/.test(mime) || /ptv/.test(q.mtype) || q.isAnimated) {
 	let out = await q.download()
-	if (/sticker/.test(mime)) out = await webp2mp4(out).catch(_ => null) || Buffer.alloc(0)
+	if (/webp/.test(mime)) out = await webp2mp4(out).catch(_ => null) || Buffer.alloc(0)
 	await conn.sendFile(m.chat, out, '', '*DONE*', m)
 	} else throw `Reply sticker / ptv with command *${usedPrefix + command}*`
 }
