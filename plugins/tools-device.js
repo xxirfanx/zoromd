e74256fe6de5b574aef55ac20218a34d39ceb4e2
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-08-30 01:09:43 +0530

    add devices

---
+++ plugins/tools-device.js
@@ -0,0 +1,11 @@
+import { getDevice } from '@adiwajshing/baileys'
+
+let handler = async (m) => {
+	m.reply(await getDevice(m.quoted ? m.quoted.id : m.key.id))
+}
+
+handler.help = ['device']
+handler.tags = ['tools']
+handler.command = /^(device)$/i
+
+export default handler
