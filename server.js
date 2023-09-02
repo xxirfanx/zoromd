8d7cc8f2b80e95e948f73999dd154610a681a7fa
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-09-03 00:17:36 +0530

    fixed bug

--- server.js
+++ server.js
@@ -1,51 +1,30 @@
 import express from 'express'
-import { createServer } from 'http'
-import path from 'path'
-import { Socket } from 'socket.io'
-import { toBuffer } from 'qrcode'
-import fetch from 'node-fetch'
+import axios from 'axios'
+import http from "http"
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
 
-function connect(conn, PORT) {
-    let app = global.app = express()
-    console.log(app)
-    let server = global.server = createServer(app)
-    // app.use(express.static(path.join(__dirname, 'views')))
-    let _qr = 'invalid'
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-    conn.ev.on('connection.update', function appQR({ qr }) {
-        if (qr) _qr = qr
-    })
+function connect(PORT) {
+  let app = global.app = express()
 
-    app.use(async (req, res) => {
-        res.setHeader('content-type', 'image/png')
-        res.end(await toBuffer(_qr))
-    })
-	
-    server.listen(PORT, () => {
-        console.log('App listened on port', PORT)
-        if (opts['keepalive']) keepAlive()
-    })
-}
-
-function pipeEmit(event, event2, prefix = '') {
-    let old = event.emit
-    event.emit = function (event, ...args) {
-        old.emit(event, ...args)
-        event2.emit(prefix + event, ...args)
-    }
-    return {
-        unpipeEmit() {
-            event.emit = old
-        }
-    }
+	app.get('/', (req, res) => {
+ res.sendFile(__dirname + '/index.html');
+});
+  
+  app.listen(process.env.port || process.env.PORT || ~~(Math.random() * 1e4), () => {
+   console.log('App listened on port', process.env.port || process.env.PORT || ~~(Math.random() * 1e4))
+   keepAlive()
+   })
 }
 
 function keepAlive() {
     const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
     if (/(\/\/|\.)undefined\./.test(url)) return
     setInterval(() => {
-        fetch(url).catch(console.error)
-    }, 5 * 1000 * 60)
+        axios(url).catch(console.error)
+    }, 5 * 1000)
 }
 
 
