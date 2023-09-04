9d797011162d057d0a1ed27bbcb4bf5e4983af0e
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-09-04 20:26:30 +0530

    skill issue

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 console.log('✅ㅤGetting started...');
 
-import {join, dirname} from 'path';
-import {createRequire} from 'module';
-import {fileURLToPath} from 'url';
-import {setupMaster, fork} from 'cluster';
-import {watchFile, unwatchFile} from 'fs';
+import { join, dirname } from 'path';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { setupMaster, fork } from 'cluster';
+import { watchFile, unwatchFile } from 'fs';
 import cfonts from 'cfonts';
-import {createInterface} from 'readline';
+import { createInterface } from 'readline';
 import yargs from 'yargs';
+
+// https://stackoverflow.com/a/50052194
+const args = [join(__dirname, 'main.js'), ...process.argv.slice(2)];
+const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+const rl = createInterface(process.stdin, process.stdout);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
-const {name, author} = require(join(__dirname, './package.json'));
-const {say} = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
+const { name, author } = require(join(__dirname, './package.json'));
+const { say } = cfonts;
 
 say('Lightweight\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot created by lua sakura`, {
+say('Bot created by lua sakura', {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
@@ -31,7 +35,6 @@ let isRunning = false;
 function start(file) {
   if (isRunning) return;
   isRunning = true;
-  const args = [join(__dirname, file), ...process.argv.slice(2)];
 
   say('Zoro md - WhatsApp Bot - baileys', {
     font: 'console',
@@ -41,41 +44,34 @@ function start(file) {
   setupMaster({
     exec: args[0],
     args: args.slice(1)});
-  const p = fork();
-  p.on('message', (data) => {
-    console.log('[ RECEIVED ]', data);
-    switch (data) {
-      case 'reset':
-        p.process.kill();
-        isRunning = false;
-        start.apply(this, arguments);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-    }
-  });
-  p.on('exit', (_, code) => {
-    isRunning = false;
-    console.error('❎ㅤAn unexpected error occurred:', code);
-
-    p.process.kill();
-    isRunning = false;
-    start.apply(this, arguments);
-
-    if (process.env.pm_id) {
-      process.exit(1);
-    } else {
-      process.exit();
-    }
-  });
-  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  let p = fork();
+	p.on('message', data => {
+		console.log('[RECEIVED]', data);
+		switch (data) {
+			case 'reset':
+				p.process.kill();
+				isRunning = false;
+				start.apply(this, arguments);
+				break;
+			case 'uptime':
+				p.send(process.uptime());
+				break;
+		}
+	});
+	p.on('exit', (_, code) => {
+		isRunning = false;
+		console.error('❎ㅤAn unexpected error occurred:', code);
+		if (code === 0) return
+		watchFile(args[0], () => {
+			unwatchFile(args[0]);
+			start(file);
+		});
+	});
   if (!opts['test']) {
-    if (!rl.listenerCount()) {
-      rl.on('line', (line) => {
-        p.emit('message', line.trim());
-      });
-    }
+    if (!rl.listenerCount()) rl.on('line', line => {
+			p.emit('message', line.trim());
+		});
   }
 }
+
 start('main.js');
