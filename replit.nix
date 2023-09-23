4336dd280285ae57d3e27b8df263716e9373b920
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2023-09-24 00:40:12 +0530

    Fix replit

--- replit.nix
+++ replit.nix
@@ -1,11 +1,11 @@
 { pkgs }: {
-	deps = [
+deps = [
     pkgs.nodejs_20
     pkgs.neofetch
-		pkgs.imagemagick
-		pkgs.speedtest-cli
-		pkgs.jellyfin-ffmpeg
-		pkgs.git
-		pkgs.python311Packages.python
-	];
+    pkgs.imagemagick
+    pkgs.speedtest-cli
+    pkgs.jellyfin-ffmpeg
+    pkgs.git
+    pkgs.python311Packages.python
+];
 }
