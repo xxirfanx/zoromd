a4dfd72fe1cf20b4fa891d8cc6a3b0296354d694
LUA SER OFC <63995922+xxirfanx@users.noreply.github.com>
2024-06-29 15:47:37 +0530

    fix antilink

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,23 +1,34 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
+/** 
+ *  Created By LUA SER OFC
+ *  CopyRight 2024 MIT License
+ *  My Github : https://github.com/xxirfanx
+ *  My Instagram : https://instagram.com/luaserofc
+ *  My Youtube : https://youtube.com/@luaserofc
+*/
 
-export async function before(m, { isAdmin, isBotAdmin }) {
-  if (m.isBaileys || !m.isGroup) return false;
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 
-  const chat = global.db.data.chats[m.chat];
-  const isGroupLink = linkRegex.exec(m.text);
-  const kickMessage = isAdmin
-    ? `❌ *Link Detected*\nYour group admin can't be removed from a group.`
-    : `❌ *Link Detected*\nYou will be removed from the group.`;
+export async function before(m, {conn, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe)
+        return !0
+    if (!m.isGroup) return !1
+    let chat = global.db.data.chats[m.chat]
+    let bot = global.db.data.settings[this.user.jid] || {}
+    const isGroupLink = linkRegex.exec(m.text)
 
-  if (chat.antiLink && isGroupLink) {
-    await this.reply(m.chat, kickMessage, null, { mentions: [m.sender] });
-    await this.sendMessage(m.chat, { delete: m.key });
-
-    if ((!isBotAdmin && isAdmin) || (isBotAdmin && !isAdmin)) {
-      await this.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
-      await this.reply(m.chat, kickMessage, null, { mentions: [m.sender] });
-      await this.sendMessage(m.chat, { delete: m.key });
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        if (isBotAdmin) {
+            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
+            if (m.text.includes(linkThisGroup)) return !0
+        }
+        await conn.reply(m.chat, `*⛽ Detected Link*
+            
+We do not allow links from other groups 
+I'm sorry *@${m.sender.split('@')[0]}* you will be expelled from the group ${isBotAdmin ? '' : '\n\n Im not an admin so I cant expel you :"v'}`, null, { mentions: [m.sender] } )
+        if (isBotAdmin && chat.antiLink) {
+        	await conn.sendMessage(m.chat, { delete: m.key })
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+        } else if (!chat.antiLink) return //m.reply('')
     }
-  }
-  return true;
+    return !0
 }
